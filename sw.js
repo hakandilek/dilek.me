cf18c339983998a6c6b901ff46661232990bbb3b
Hakan Dilek <hakandilek@gmail.com>
2019-06-30 17:58:52 +0200

    Built from 92b1b9b

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20190630160126";
+var CACHE_NAME = "pixyll2-20190630183650";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201906301601",
+        "/css/pixyll.css?201906301836",
         "/"
       ]);
     })
